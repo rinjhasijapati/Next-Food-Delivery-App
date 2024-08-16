757e13888946b76cf086c9d45a23031303d52dae
rinjhasijapati <aastha_sijapati@yahoo.com>
2024-08-16 22:01:37 +0545

    pages and components

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MenuPage = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default MenuPage
---
+++ src/app/orders/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const OrdersPage = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default OrdersPage
